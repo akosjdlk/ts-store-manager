e4d2c362cd13de15fe419cd951103ed8c08da618
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2026-05-21 13:43:11 +0200

    stocking base

--- src/components/stockup.ts
+++ src/components/stockup.ts
@@ -1,3 +1,18 @@
 import { initFlowbite } from 'flowbite'
 
+import { fetchProductById, updateProduct } from '../api/products';
+
 initFlowbite()
+
+const stockupSubmitBtn = document.getElementById('stockup_submit_btn') as HTMLButtonElement
+const add_stockup_form = document.getElementById('add_stockup_form') as HTMLFormElement
+stockupSubmitBtn.addEventListener('click', async () => {
+        const productBarcode = (document.getElementById('barcode') as HTMLInputElement).value;
+        const productQuantity = Number((document.getElementById('quantity') as HTMLInputElement).value);
+
+        const product = await fetchProductById(productBarcode);
+        product.keszlet += productQuantity;
+        await updateProduct(product.id, product);
+
+        add_stockup_form.reset();
+});
