9d8c41fc4283b8613b0fff27c238ad26b682a241
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2026-05-21 13:43:11 +0200

    stocking base

--- src/components/stockup.ts
+++ src/components/stockup.ts
@@ -2,36 +2,19 @@ import { initFlowbite } from 'flowbite'
 
 import { fetchProductById, updateProduct } from '../api/products';
 
+import { fetchProductById, updateProduct } from '../api/products';
+
 initFlowbite()
 
 const stockupSubmitBtn = document.getElementById('stockup_submit_btn') as HTMLButtonElement
 const add_stockup_form = document.getElementById('add_stockup_form') as HTMLFormElement
-
-const editedProducts: Array<{ barcode: string, currentQuantity: number, addToQuantity: number }> = []
-
 stockupSubmitBtn.addEventListener('click', async () => {
         const productBarcode = (document.getElementById('barcode') as HTMLInputElement).value;
-        const addToProductQuantity = Number((document.getElementById('quantity') as HTMLInputElement).value);
+        const productQuantity = Number((document.getElementById('quantity') as HTMLInputElement).value);
 
         const product = await fetchProductById(productBarcode);
-        
-        editedProducts.push({ barcode: productBarcode, currentQuantity: product.keszlet, addToQuantity: addToProductQuantity });
-        localStorage.setItem('editedProducts', JSON.stringify(editedProducts));
+        product.keszlet += productQuantity;
+        await updateProduct(product.id, product);
 
         add_stockup_form.reset();
 });
-
-const confirmStockupBtn = document.getElementById('confirmstockupbtn') as HTMLButtonElement;
-confirmStockupBtn.addEventListener('click', async () => {
-        const editedProductsFromStorage = localStorage.getItem('editedProducts');
-        if (editedProductsFromStorage) {
-                const productsToUpdate = JSON.parse(editedProductsFromStorage) as Array<{ barcode: string, currentQuantity: number, addToQuantity: number }>;
-                for (const product of productsToUpdate) {
-                        const productDetails = await fetchProductById(product.barcode);
-                        productDetails.keszlet += product.addToQuantity;
-
-                        await updateProduct( product.barcode, productDetails );
-                }
-                localStorage.removeItem('editedProducts');
-        }
-});    
