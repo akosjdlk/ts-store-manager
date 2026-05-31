5d039db853e961212357aa721e64752faba4002f
rbarbi207 <rozs.barbara@students.jedlik.eu>
2026-05-31 22:16:27 +0200

    remove comment

--- src/components/table.ts
+++ src/components/table.ts
@@ -200,37 +200,7 @@ export function createDataTable<T extends Product | Sale>(
         });
     }
 
-    // function renderSortIcons(): void {
-    //     const thElements = table.querySelectorAll("th");
-        
-    //     thElements.forEach((th, index) => {
-    //         if ((deleteButton || modifyButtonCallback) && index === thElements.length - 1) {
-    //             return;
-    //         }
-
-    //         if (th.querySelector(".custom-sort-icon")) {
-    //             return;
-    //         }
-
-    //         const sorter = th.querySelector(".datatable-sorter");
-
-    //         if (sorter) {
-    //             sorter.classList.add("flex", "items-center", "justify-center", "w-full", "gap-2");
-
-    //             const svgContainer = document.createElement("span");
-    //             svgContainer.className = "custom-sort-icon shrink-0";
-    //             svgContainer.innerHTML = `
-    //                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    //                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"></path>
-    //                 </svg>
-    //             `;
-    //             sorter.appendChild(svgContainer);
-    //         }
-    //     });
-    // }
-
     function refreshTableUI(): void {
-        // renderSortIcons();
         styleZeroStockRows();
     }
 
