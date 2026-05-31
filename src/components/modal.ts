e2779a1eca0e6109f7c0af282eb1ff3c8bf371e0
rbarbi207 <rozs.barbara@students.jedlik.eu>
2026-06-01 00:52:09 +0200

    confirmation-modal

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -1,4 +1,5 @@
 import type { DataTable } from "simple-datatables";
+import "flowbite"
 
 // TODO: another modal for modify + add new thing
 // TODO: toast add new thing + modify
@@ -247,4 +248,50 @@ export class CustomModal {
       this.modalEl.remove();
     }, 10);
   }
+}
+
+export function confirmationModal(message: string, ontrue: CallableFunction, onfalse: CallableFunction): void  {
+  const popup = document.getElementById('popup-modal')!;
+  popup.classList.add("hidden", "overflow-y-auto", "overflow-x-hidden", "fixed", "top-0", "right-0", "left-0", "z-50", "justify-center", "items-center", "w-full", "md:inset-0", "h-[calc(100%-1rem)]", "max-h-full");
+  popup.classList.remove("hidden");
+  popup.innerHTML = 
+  `<div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
+                <button type="button" class="absolute top-3 end-2.5 text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="popup-modal">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            <div class="p-4 md:p-5 text-center">
+                <svg class="mx-auto mb-4 text-fg-disabled w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>
+                <h3 class="mb-6 text-body">${message}</h3>
+                <div class="flex items-center space-x-4 justify-center">
+                    <button data-modal-hide="popup-modal" data-result="1" type="button" class="text-white bg-danger box-border border border-transparent hover:bg-danger-strong focus:ring-4 focus:ring-danger-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+                    Igen
+                    </button>
+                    <button data-modal-hide="popup-modal" data-result="0" type="button" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Nem, mégse</button>
+                </div>
+            </div>
+        </div>
+    </div>`
+  
+  popup.querySelectorAll<HTMLButtonElement>('button[data-modal-hide="popup-modal"]').forEach(
+    (btn) => {
+      btn.addEventListener("click", () => {
+        popup.classList.add("hidden")
+      })
+    }
+  )
+  popup.querySelectorAll<HTMLButtonElement>("button[data-result]").forEach(
+    (btn) => {
+      btn.addEventListener("click", () => {
+        if (btn.dataset["result"] === "1") {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          ontrue() 
+        } else {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          onfalse()
+        }
+      })
+    }
+  )
 }
