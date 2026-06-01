906f0e5071a83818532ddb32d743bf1216546dcc
rbarbi207 <rozs.barbara@students.jedlik.eu>
2026-06-01 20:46:09 +0200

    modal position fixed + confirmation-modal

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -252,7 +252,7 @@ export class CustomModal {
 
 export function confirmationModal(message: string, ontrue: CallableFunction, onfalse: CallableFunction): void  {
   const popup = document.getElementById('popup-modal')!;
-  popup.classList.add("hidden", "overflow-y-auto", "overflow-x-hidden", "fixed", "top-0", "right-0", "left-0", "z-50", "justify-center", "items-center", "w-full", "md:inset-0", "h-[calc(100%-1rem)]", "max-h-full");
+  popup.classList.add("hidden", "flex", "overflow-y-auto", "overflow-x-hidden", "fixed", "top-0", "right-0", "left-0", "z-50", "justify-center", "items-center", "w-full", "md:inset-0", "h-[calc(100%-1rem)]", "max-h-full");
   popup.classList.remove("hidden");
   popup.innerHTML = 
   `<div class="relative p-4 w-full max-w-md max-h-full">
